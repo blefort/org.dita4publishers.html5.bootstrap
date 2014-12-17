a4fe9da16392bf475aabd2b32f44dcd57661f496
Bertrand Lefort <blefort@uottawa.ca>
2014-12-17 15:20:07 -0500

    Implements swipe event, need attention

--- theme/html5-bootstrap/d4p-boostrap/js/script.js
+++ theme/html5-bootstrap/d4p-boostrap/js/script.js
@@ -1,6 +1,9 @@
 // add div for facebook
 $(function() {
 
+  /**
+   * Fix header
+   */
   var header = document.getElementById('site-head'),
   headerHeadRoom =  new Headroom(header, {
     offset: 205,
@@ -28,12 +31,29 @@ $(function() {
     , 600);
   });
 
-  $('body').scrollspy({ target: '#side-navigation' })
-  $('abbr').popover({delay: { "show": 500, "hide": 100 }});
-  $('html').click(function() {
-    $('.popover').popover('hide');
-  });
 
+  /**
+   * Navigation
+   */
+  if(!$('body').hasClass('homepage'))
+  {
+    navigation.init();
+  }
+
+  //$('body').scrollspy({ target: '#side-navigation' })
+
+  /**
+   * popover
+   */
+ // $('abbr').popover({delay: { "show": 500, "hide": 100 }});
+ // $('html').click(function() {
+ //   $('.popover').popover('hide');
+//  });
+
+
+  /**
+   * Serach index
+   */
   var idx = new searchIdx(),
   closeBtn = $('<button />').attr('id', 'searchClose').attr('class', 'float_right').append($('<span />').attr('class', 'fa fa-close')).append($('<span />').html(d4p.l.close).attr('class', 'hidden')).hide();
 
@@ -62,6 +82,22 @@ $(function() {
       $('#search_result').show();
       $('#searchClose').show();
     }
+  });
+
+  $("body").swipe( {
+    //Generic swipe handler for all directions
+    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+      if(duration == swipeLeft && d4p.previousTopicHref !== null && d4p.previousTopicHref !== '')
+      {
+        document.location = d4p.getDocumentationRoot + d4p.previousTopicHref;
+      }
+      if(duration == swipeRight && d4p.nextTopicHref !== null && d4p.nextTopicHref !== '')
+      {
+        document.location = d4p.getDocumentationRoot + d4p.nextTopicHref;
+      }
+    },
+    //Default is 75px, set to 0 for demo so any distance triggers swipe
+    threshold:30
   });
 
 });
